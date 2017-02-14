80e257320f0fe73dccf4f4e4c5a7b0eae5fdb975
Gergely Kralik <hello@kralik.io>
2017-02-14 14:55:34 +0100

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        "indent": ["error", 4 ],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "single", "avoid-escape"],
+        "semi": ["error", "always"],
+        "space-after-function-name": [1, "always"],
+		"space-before-blocks": [1, "always"],
+    }
+};
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+ESLint Shared Configuration file KRAGA
+======================================
+
+@version 1.0.0
+
+@author Gergely Králik <gergely.kralik@kraga.sk>
